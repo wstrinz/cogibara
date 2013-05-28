a28751e0d8b354be2dde5cbf12aae0fa82479697
wstrinz <wstrinz@gmail.com>
2013-05-28 18:04:34 -0500

    adding config defaults

--- lib/cogibara/default_config.rb
+++ lib/cogibara/default_config.rb
@@ -1,6 +1,6 @@
 require 'yaml'
 Cogibara.setup_dispatcher do |dispatcher|
-  # yml = YAML.load_file('/home/wstrinz/Documents/Projects/speechrecog/config.yml')
+  yml = YAML.load_file('./default_config.yml')
   # dispatcher.config_from_yaml(yml)
   # dispatcher.register_operator(["REMINDER"],{name: "Reminder Setter"})
 end
